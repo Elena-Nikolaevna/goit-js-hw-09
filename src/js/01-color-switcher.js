555cc1848a4f40b0e945c2711af152fd830109b8
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-08-12 01:51:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const TIME_SHOW = 1000;
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
 
+let interval = 0;
+
+refs.start.addEventListener('click', () => {
+  console.log('start');
+
+  randomColors(refs.body);
+
+  interval = setInterval(() => {
+    randomColors(refs.body);
+  }, TIME_SHOW);
+});
+
+refs.stop.addEventListener('click', () => {
+  console.log('stop');
+  clearInterval(interval);
+});
+
+function randomColors(e) {
+  e.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
